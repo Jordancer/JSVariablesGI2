04f25739de2a40e5d2184ba0a91bbb6fb477de9b
Jordan Dancer <112099523+Jordancer@users.noreply.github.com>
2022-09-21 01:02:08 -0400

    Easy Completed

--- Easy/app.js
+++ Easy/app.js
@@ -1,5 +1,13 @@
-const student1 = {fName:"Jordan", lName:"Dancer", age:"25" }
-const student2 = {fName:"Jade", lName:"Dancer", age:"3" }
-const student3 = {fName:"Langston", lName:"Kodzai", age:"25" }
+const student1 = { fName: "Jordan", lName: "Dancer", age: "25" };
+const student2 = { fName: "Jade", lName: "Dancer", age: "3" };
+const student3 = { fName: "Langston", lName: "Kodzai", age: "25" };
 
-console.log("Hello, My Name Is " + student2.fName + " " + student2.lName + " and Im " + student2.age + " Years Old");
+console.log(
+  "Hello, My Name Is " +
+    student2.fName +
+    " " +
+    student2.lName +
+    " and Im " +
+    student2.age +
+    " Years Old"
+);
